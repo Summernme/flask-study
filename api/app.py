ac5c392ece4cbffe9d9bea8534e8464e121bb5a6
Summernme <cie654@naver.com>
2023-02-21 01:01:45 +0900

    max retry...

--- api/app.py
+++ api/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
+from time import sleep
 
 app             = Flask(__name__)
 app.id_count    = 1
 app.users       = {}
+app.tweets      = []
 
 @app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
 
+# 회원가입
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
     new_user                = request.json
@@ -16,3 +19,23 @@ def sign_up():
     app.id_count            = app.id_count + 1
 
     return jsonify(new_user)
+
+@app.route("/tweet", methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet   = payload['tweet'])
+
+    if user.id not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+
+    if len(tweet) > 300 :
+        return "300자를 초과했습니다.", 400
+
+    user_id = int[payload['id'])
+    app.tweets.append({
+        'user_id' : user_id,
+        'tweet'   : tweet
+    })
+    
+    return '', 200
